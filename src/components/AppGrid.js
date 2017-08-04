e0ffacb5ebb8dc80e91303537cdd2c7d2a5f1138
Collins Orlando <sleekcollins@live.com>
2017-08-04 19:12:11 +0100

    Added App Grid

---
+++ src/components/AppGrid.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class AppGrid extends React.Component {
+	render() {
+		return (
+			<div className="grid">
+				<div><span>X</span></div>
+				<div><span>X</span></div>
+				<div><span>O</span></div>
+				<div><span>X</span></div>
+				<div><span>O</span></div>
+				<div><span>X</span></div>
+				<div><span>X</span></div>
+				<div><span>O</span></div>
+				<div><span>O</span></div>
+			</div>
+		);
+	}
+}
+
+export default AppGrid;
